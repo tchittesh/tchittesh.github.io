30c1294fe7bcfc7c45ceecba906721b5ae0d1f6f
Chittesh Thavamani <tchittesh@gmail.com>
2023-03-06 10:48:10 +0530

    Increase animation speeds, update favicon, make project cards smaller

--- js/fade.js
+++ js/fade.js
@@ -5,20 +5,20 @@ $(document).ready(function() {
 
     /* Check the location of each desired element */
     $('.fade-up').each( function(){
-      var middle_of_object = $(this).position().top + $(this).outerHeight()/2;
+      var top_of_object = $(this).position().top; // + $(this).outerHeight()/2;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       /* If the object is completely visible in the window, fade it it */
-      if( bottom_of_window > middle_of_object ){
-        $(this).animate({opacity:1, top: '0px'}, 1200);
+      if( bottom_of_window > top_of_object ){
+        $(this).animate({opacity:1, top: '0px'}, 600);
       }
     });
 
     $('.fade-left').each( function(){
-      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var top_of_object = $(this).position().top; // + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       /* If the object is completely visible in the window, fade it it */
-      if( bottom_of_window > bottom_of_object ){
-        $(this).animate({opacity:1, left: '0px'}, 800);
+      if( bottom_of_window > top_of_object ){
+        $(this).animate({opacity:1, left: '0px'}, 600);
       }
     });
   });
